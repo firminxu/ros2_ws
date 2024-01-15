086f7791244effdbc011bacf889815988a78d693
firminxu <xufengming@gmail.com>
2024-01-15 18:46:16 +0800

    新文件：   src/more_interfaces/CMakeLists.txt 	新文件：   src/more_interfaces/LICENSE 	新文件：   src/more_interfaces/msg/AddressBook.msg 	新文件：   src/more_interfaces/package.xml 	新文件：   src/my_py_srvcli/LICENSE 	新文件：   src/my_py_srvcli/my_py_srvcli/__init__.py 	新文件：   src/my_py_srvcli/my_py_srvcli/client_member_function.py 	新文件：   src/my_py_srvcli/my_py_srvcli/service_member_function.py 	新文件：   src/my_py_srvcli/package.xml 	新文件：   src/my_py_srvcli/resource/my_py_srvcli 	新文件：   src/my_py_srvcli/setup.cfg 	新文件：   src/my_py_srvcli/setup.py 	新文件：   src/my_py_srvcli/test/test_copyright.py 	新文件：   src/my_py_srvcli/test/test_flake8.py 	新文件：   src/my_py_srvcli/test/test_pep257.py 	修改：     src/py_pubsub/package.xml 	修改：     src/py_pubsub/py_pubsub/publisher_member_function.py 	修改：     src/py_pubsub/py_pubsub/subscriber_member_function.py 	修改：     src/py_srvcli/package.xml 	修改：     src/py_srvcli/py_srvcli/client_member_function.py 	修改：     src/py_srvcli/py_srvcli/service_member_function.py 	新文件：   src/python_parameters/LICENSE 	新文件：   src/python_parameters/launch/python_parameters_launch.py 	新文件：   src/python_parameters/package.xml 	新文件：   src/python_parameters/python_parameters/__init__.py 	新文件：   src/python_parameters/python_parameters/python_parameters_node.py 	新文件：   src/python_parameters/resource/python_parameters 	新文件：   src/python_parameters/setup.cfg 	新文件：   src/python_parameters/setup.py 	新文件：   src/python_parameters/test/test_copyright.py 	新文件：   src/python_parameters/test/test_flake8.py 	新文件：   src/python_parameters/test/test_pep257.py

--- src/my_py_srvcli/my_py_srvcli/client_member_function.py
+++ src/my_py_srvcli/my_py_srvcli/client_member_function.py
@@ -1,7 +1,5 @@
+from tutorial_interfaces.srv import MultipleTwoInts
 import sys
-
-# from example_interfaces.srv import AddTwoInts
-from tutorial_interfaces.srv import AddThreeInts
 import rclpy
 from rclpy.node import Node
 
@@ -10,39 +8,36 @@ class MinimalClientAsync(Node):
 
     def __init__(self):
         super().__init__('minimal_client_async')
-        self.cli = self.create_client(AddThreeInts, 'add_three_ints')
+        self.cli = self.create_client(MultipleTwoInts, 'multiple_two_ints')       # CHANGE
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
-        self.req = AddThreeInts.Request()
+        self.req = MultipleTwoInts.Request()                                   # CHANGE
 
     def send_request(self):
         self.req.a = int(sys.argv[1])
         self.req.b = int(sys.argv[2])
-        self.req.c = int(sys.argv[3])
         self.future = self.cli.call_async(self.req)
-        # rclpy.spin_until_future_complete(self, self.future)
-        # return self.future.result()
 
 
-def main():
-    rclpy.init()
-    minimal_client=MinimalClientAsync()
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_client = MinimalClientAsync()
     minimal_client.send_request()
-    
+
     while rclpy.ok():
         rclpy.spin_once(minimal_client)
         if minimal_client.future.done():
             try:
-                response=minimal_client.future.result()
-            
+                response = minimal_client.future.result()
             except Exception as e:
-                minimal_client.get_logger().info('Service call failed %r' % (e,))
-                break
-            
+                minimal_client.get_logger().info(
+                    'Service call failed %r' % (e,))
             else:
                 minimal_client.get_logger().info(
-                    'Result of add_three_ints: for %d + %d + %d= %d' %
-                    (minimal_client.req.a, minimal_client.req.b, minimal_client.req.c, response.sum))
+                    'Result of multiple_two_ints: for %d * %d = %d' %                                # CHANGE
+                    (minimal_client.req.a, minimal_client.req.b, response.product))  # CHANGE
+            break
 
     minimal_client.destroy_node()
     rclpy.shutdown()
